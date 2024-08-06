4c4fb344b0366d336e5f9229d2ab4135680dd90c
eps1lon <sebastian.silbermann@vercel.com>
2024-08-06 21:21:12 +0200

    Fix missing keys in Fragments in arrays

--- packages/next/src/server/app-render/app-render.tsx
+++ packages/next/src/server/app-render/app-render.tsx
@@ -171,6 +171,8 @@ interface ParseRequestHeadersOptions {
   readonly isRoutePPREnabled: boolean
 }
 
+const flightDataPathHeadKey = 'flight-data-path-head'
+
 interface ParsedRequestHeaders {
   /**
    * Router state provided from the client-side router. Used to handle rendering
@@ -368,11 +370,11 @@ async function generateDynamicRSCPayload(
         isFirst: true,
         // For flight, render metadata inside leaf page
         rscPayloadHead: (
-          <>
+          <React.Fragment key={flightDataPathHeadKey}>
             <NonIndex ctx={ctx} />
             {/* Adding requestId as react key to make metadata remount for each render */}
             <MetadataTree key={requestId} />
-          </>
+          </React.Fragment>
         ),
         injectedCSS: new Set(),
         injectedJS: new Set(),
@@ -522,11 +524,11 @@ async function getRSCPayload(
     typeof varyHeader === 'string' && varyHeader.includes(NEXT_URL)
 
   const initialHead = (
-    <>
+    <React.Fragment key={flightDataPathHeadKey}>
       <NonIndex ctx={ctx} />
       {/* Adding requestId as react key to make metadata remount for each render */}
       <MetadataTree key={ctx.requestId} />
-    </>
+    </React.Fragment>
   )
 
   return {
@@ -539,7 +541,7 @@ async function getRSCPayload(
     f: [[initialTree, seedData, initialHead]],
     m: missingSlots,
     G: GlobalError,
-  } satisfies RSCPayload & { P: React.ReactNode }
+  } satisfies InitialRSCPayload & { P: React.ReactNode }
 }
 
 /**
